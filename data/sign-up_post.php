2c2f037c471fdf6c70808f5ca9e1c263984e7ef3
Steven Kobza <steven_kobza@sfu.ca>
2020-10-23 11:40:49 -0700

    added in some info

--- data/sign-up_post.php
+++ data/sign-up_post.php
@@ -26,6 +26,27 @@
 
         <main>
             <?php
+            include("../phpData/logins.php");
+            $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+            if ($mysqli->connect_errno) {
+                echo "Failed to connect to MySQL: " . $mysqli->connect_error; 
+            }
+            
+            $createUserQuery = "CREATE TABLE users (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(30) NOT NULL,
+                password CHAR(128) NOT NULL,
+                email VARCHAR(128) NOT NULL
+            )";
+
+            if (!$mysqli->query("DROP TABLE IF EXISTS users") || !$mysqli->query($createUserQuery)) {
+                echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            }
+
+            if (!($stmt = $mysqli->prepare("INSERT INTO test(id) VALUES (?)"))) {
+                echo "Preare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            }
+
             class User {
                 public $email;
                 public $username;
