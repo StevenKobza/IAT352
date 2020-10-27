e0594b1ecda3b1e8b700b298eb94b8426fe66ce7
Steven Kobza <steven_kobza@sfu.ca>
2020-10-27 14:08:56 -0700

    adding the ability to create new stuff

--- data/createDataset.php
+++ data/createDataset.php
@@ -1,22 +1,51 @@
 <?php
-include 'logins.php';
-$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error; 
-}
-            
-$createUserQuery = "CREATE TABLE users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    password CHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-)";
+function addPlayers() {
+    include 'logins.php';
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error; 
+    }
+                
+    $createPlayerQuery = "CREATE TABLE players (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        playerName VARCHAR(30) NOT NULL,
+        position CHAR(4) NOT NULL,
+        club VARCHAR(128) NOT NULL,
+        league VARCHAR(128) NOT NULL,
+        pace TINYINT(2) UNSIGNED NOT NULL,
+        shooting TINYINT(2) UNSIGNED NOT NULL,
+        passing TINYINT(2) UNSIGNED NOT NULL,
+        dribbling TINYINT(2) UNSIGNED NOT NULL,
+        defense TINYINT(2) UNSIGNED NOT NULL,
+        physical TINYINT(2) UNSIGNED NOT NULL,
+        cardRating TINYINT(2) UNSIGNED NOT NULL,
+        weakFoot TINYINT(1) UNSIGNED NOT NULL,
+        skillMoves TINYINT(1) UNSIGNED NOT NULL,
+        workRates VARCHAR(255) NOT NULL,
+        strongFoot VARCHAR(5) NOT NULL
+    )";
 
-if (!$mysqli->query("DROP TABLE IF EXISTS users") || !$mysqli->query($createUserQuery)) {
-    echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
+    $createClubQuery = "CREATE TABLE clubs (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        clubName VARCHAR(128) NOT NULL,
+        league VARCHAR(128) NOT NULL
+    )";
 
-if (!($stmt = $mysqli->prepare("INSERT INTO users(username, password, email) VALUES (?, ?, ?)"))) {
-    echo "Preare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    $createPlaysForQuery = "CREATE TABLE playsFor (
+        player_id INT(6) NOT NULL,
+        club_id INT(6) NOT NULL,
+        FOREIGN_KEY(player_id) REFERENCES players(id),
+        FOREIGN_KEY(club_id) REFERENCES clubs(id)
+    )";
+
+    $createFavouritePlayersQuery;
+
+    if (!$mysqli->query("DROP TABLE IF EXISTS players") || !$mysqli->query($createPlayerQuery)) {
+        echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    /*if (!($stmt = $mysqli->prepare("INSERT INTO users(username, password, email) VALUES (?, ?, ?)"))) {
+        echo "Preare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }*/
 }
 ?>
