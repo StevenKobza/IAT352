87ec735e98683cde3fb1a766908f2bc7c0761e51
Steven Kobza <steven_kobza@sfu.ca>
2020-11-11 14:03:44 -0800

    changing up aka fixing part of the sign up

--- data/sign-up_post.php
+++ data/sign-up_post.php
@@ -32,23 +32,11 @@
                 echo "Failed to connect to MySQL: " . $mysqli->connect_error; 
             }
             
-            $createUserQuery = "CREATE TABLE users (
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                username VARCHAR(30) NOT NULL,
-                password CHAR(128) NOT NULL,
-                email VARCHAR(128) NOT NULL
-            )";
-
-            if (!$mysqli->query("SELECT * FROM users")) {
-                if (!$mysqli->query("DROP TABLE IF EXISTS users") || !$mysqli->query($createUserQuery)) {
-                    echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-                }
-            }
-            if (!($stmt = $mysqli->prepare("INSERT INTO users(username, password, email) VALUES (?, ?, ?)"))) {
+            if (!($stmt = $mysqli->prepare("INSERT INTO users(username, email, password) VALUES (?, ?, ?)"))) {
                 echo "Preare failed: (" . $mysqli->errno . ") " . $mysqli->error;
             }
 
-            $stmt->bind_param("sss", $username, $password, $email);
+            $stmt->bind_param("sss", $username, $email, $password);
 
             $username = $_POST["username"];
             $password = $_POST["password"];
